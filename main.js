55347fd6db897e36ad93e939fc9710fd74f633eb
Daniel Jackson <daniel@cloakedninjas.co.uk>
2017-06-21 14:28:56 +0100

    Particles

--- main.js
+++ main.js
@@ -4,7 +4,26 @@ function App () {
   this.result = document.getElementById('result');
   this.houseName = document.getElementById('housename');
 
+  this.canvas = document.getElementById('canvas');
+  this.ctx = this.canvas.getContext('2d');
+  this.particles = [];
+
+  this.width = this.canvas.width = window.innerWidth;
+  this.height = this.canvas.height = window.innerHeight;
+  this.mouse = { x: this.width * 0.5, y: this.height * 0.5 };
+
   document.getElementById('button').addEventListener('click', this.handleFormSubmit.bind(this));
+
+  for (var i = 0; i <= 200; i++) {
+    this.particles[i] = new Particle(this.ctx, Math.random() * this.width, Math.random() * this.height);
+  }
+
+  document.body.addEventListener('mousemove', function (e) {
+    this.mouse.x = e.clientX;
+    this.mouse.y = e.clientY;
+  }.bind(this));
+
+  requestAnimationFrame(this.frameTick.bind(this));
 
   this.input.focus();
 }
@@ -45,6 +64,18 @@ App.prototype = {
     }
 
     return sum % 4;
+  },
+
+  frameTick: function () {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+
+    for (var i = 0; i < this.particles.length; i++) {
+      this.particles[i].attract(this.mouse.x, this.mouse.y);
+      this.particles[i].integrate();
+      this.particles[i].draw();
+    }
+
+    requestAnimationFrame(this.frameTick.bind(this));
   }
 };
 
---
+++ particle.js
@@ -0,0 +1,48 @@
+var DAMPING = 0.99;
+
+function Particle(ctx, x, y) {
+  this.ctx = ctx;
+  this.x = this.oldX = x;
+  this.y = this.oldY = y;
+}
+
+Particle.prototype.integrate = function() {
+  var velocityX = (this.x - this.oldX) * DAMPING;
+  var velocityY = (this.y - this.oldY) * DAMPING;
+
+  this.oldX = this.x;
+  this.oldY = this.y;
+  this.x += velocityX;
+  this.y += velocityY;
+};
+
+Particle.prototype.attract = function(x, y) {
+  var dx = x - this.x;
+  var dy = y - this.y;
+  var distance = Math.sqrt(dx * dx + dy * dy);
+
+  console.log(distance);
+
+  if (distance < 1) {
+    var invX = Math.random() > 0.5 ? 1 : -1;
+    var invY = Math.random() > 0.5 ? 1 : -1;
+
+    this.x = x + (Math.random() * 200 * invX);
+    this.y = y + (Math.random() * 200 * invY);
+    this.oldX = this.x;
+    this.oldY = this.y;
+  } else {
+    this.x += dx / distance;
+    this.y += dy / distance;
+  }
+
+};
+
+Particle.prototype.draw = function() {
+  this.ctx.strokeStyle = 'silver';
+  this.ctx.lineWidth = 2;
+  this.ctx.beginPath();
+  this.ctx.moveTo(this.oldX, this.oldY);
+  this.ctx.lineTo(this.x, this.y);
+  this.ctx.stroke();
+};
