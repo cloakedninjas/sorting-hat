1235e991b06a57699c098c78d03e1322c6be8769
Daniel Jackson <daniel@cloakedninjas.co.uk>
2017-06-21 11:25:43 +0100

    More WIP

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 function App () {
   this.input = document.getElementById('name');
   this.form = document.getElementById('form');
+  this.result = document.getElementById('result');
+  this.houseName = document.getElementById('housename');
 
   document.getElementById('button').addEventListener('click', this.handleFormSubmit.bind(this));
+
+  this.input.focus();
 }
 
 App.prototype = {
   handleFormSubmit: function (e) {
-    var result = this.encodeString(this.input);
+    var result,
+        isForced = App.FORCED[this.input.value];
+
+    if (isForced) {
+      result = isForced;
+    } else {
+      result = this.encodeString(this.input.value);
+    }
+    var houseResult = App.HOUSES[result];
 
     e.preventDefault();
-    document.body.classList.add(App.HOUSES[result]);
+    e.stopPropagation();
+    document.body.classList.add(houseResult);
 
-    form.classList.add('fade-out');
+    this.form.classList.add('fade-out');
+    this.result.style.display = 'block';
+    this.houseName.textContent = houseResult;
+
+    setTimeout(function () {
+
+      this.result.classList.add('fade-in');
+    }.bind(this), 1000);
+
   },
 
   encodeString: function (input) {
     var sum = 0,
-        letters = input.value.split('');
+        letters = input.split('');
 
     for(var i = 0, len = letters.length; i < len; i++) {
       sum += letters[i].charCodeAt(0);
@@ -28,6 +49,11 @@ App.prototype = {
 };
 
 App.HOUSES = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'];
+App.FORCED = {
+  'Ella': 3,
+  'Cynthia': 3,
+  'Will': 1
+};
 
 document.addEventListener('DOMContentLoaded', function () {
   new App();
